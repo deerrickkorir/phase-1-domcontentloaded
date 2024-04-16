dbdbef0d11c56836251444cf581ffcfe66434599
Derrick korir kiptoo <derrickroz4227@gmail.com>
2024-04-17 02:03:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Target the paragraph element with id "text"
+    const textElement = document.getElementById("text");
+  
+    // Replace the text content with "This is really cool!"
+    textElement.textContent = "This is really cool!";
+  });
+  
